b73ca18976223aeb0330001132e2dc96773a65fb
anglehuanhuan <genghuan1205@126.com>
2016-12-31 13:07:27 +0800

    test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('./styles/app.scss');
 
 var common = require('./scripts/utils/utils.common.js');
-var html = require('./scripts/tpls/index.html');
+var html = require('./scripts/tpls/index.string');
 common.render(html);
 
 require('./scripts/views/index.js')
